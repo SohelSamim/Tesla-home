e91010b6c2c08da28f1c8c27718dda0af4cd834c
Sohel Samimi <47162719+SohelSamim@users.noreply.github.com>
2021-10-23 01:40:58 +0200

    header and logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1> Sohel Samimi</h1>
+      <Header/>
+      
+      {/*items => snapping */}
+      {/*items => snapping */}
+      {/*items => snapping */}
+      {/*items => snapping */}
 
     </div>
   );
