444214ca8f9f5e795b4ceac691c2a2e790500d93
Sohel Samimi <47162719+SohelSamim@users.noreply.github.com>
2021-10-24 00:04:52 +0200

    css/images done

---
+++ src/components/Button.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Button = () => {
+    return (
+        <div>
+            <div className="button">
+            </div>
+        </div>
+    )
+}
+
+export default Button
--- src/components/Header.js
+++ src/components/Header.js
@@ -12,7 +12,6 @@ const Header = () => {
             </div>
 
             <div className="header__center">
-
                 <p> Model S </p>
                 <p> Model 3 </p>
                 <p> Model X </p>
